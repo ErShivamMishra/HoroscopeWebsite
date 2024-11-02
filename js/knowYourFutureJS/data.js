21898946e7a193edef99433f54a3ba0c342012df
Shivam Kumar Mishra <er.shivamm01@gmial.com>
2024-11-02 07:17:57 +0530

    Initial Commit

---
+++ js/knowYourFutureJS/data.js
@@ -0,0 +1,133 @@
+var arr=[{
+  zid:1,
+  zname:'Aries',
+  zelement:'fire',
+  zcolour:'Fiery red',
+  zdigit:9,
+  zdescrption:'People of this zodiac sign are emotional, dynamic and temperamental and get angry easily. People of the fire zodiac are courageous, energetic and idealistic.',
+  zdone:'Feed roasted garm and jeggery to the monkey on tuesday,Serve elder brother and sister,Donate Whole lantils to the Mars',
+  zplanet:'Mars',
+  img:'h1.svg'
+},
+{
+  zid:2,
+  zname:'Taurus',
+  zelement:'Earth',
+  zcolour:'Pink and white',
+  zdigit:6,
+  zdescrption:'People of Earth zodiac sign are down to earth, practical and trustworthy. People of this zodiac sign have attachment to material things.A feeling of laziness can be seen in the people of these zodiac signs. These do not move easily from their place.',
+  zdone:'Serve the Cow,Keep home and body clean,Donate Curd,Ghee and Comphore',
+  zplanet:'Venus',
+  img:'h2.svg'
+},
+{
+  zid:3,
+  zname:'Gemini',
+  zelement:'Air',
+  zcolour:'yellow and green',
+  zdigit:5,
+  zdescrption:'People of this zodiac sign are intellectuals, sociable, thinkers, and analysts. People of Air sign enjoy reading books',
+  zdone:'Make a hole in copper penny and throw it in flowing water,Donate whole moong,Worship Mother Durga',
+  zplanet:'Mercury',
+  img:'h3.svg'
+},
+{
+  zid:4,
+  zname:'Cancer',
+  zelement:'Water',
+  zcolour:'white, grey, silver and cream',
+  zdigit:2,
+  zdescrption:'People of water zodiac sign are emotional and sensitive and have sharp memory power and are always ready to help their loved ones.',
+  zdone:'Keep raw Milk and Rice in a temple for a few days,Serve Mother,Donate rice milk and silver Item for Chandra',
+  zplanet:'Moon',
+  img:'h4.svg'
+},
+{
+  zid:5,
+  zname:'Leo',
+  zelement:'Fire',
+  zcolour:'gold, regal purple and burnt orange',
+  zdigit:1,
+  zdescrption:'People of this zodiac sign are emotional, dynamic and temperamental and get angry easily. People of the fire zodiac are courageous, energetic and idealistic.A feeling of laziness can be seen in the people of these zodiac signs. These do not move easily from their place.',
+  zdone:'Give water to the Sun,Serve the Father,Donate wheat and copper Utensils',
+  zplanet:'Sun',
+  img:'h5.svg'
+},
+{
+  zid:6,
+  zname:'Virgo',
+  zelement:'Earth',
+  zcolour:'blue, green, yellow and white',
+  zdigit:5,
+  zdescrption:'People of Earth zodiac sign are down to earth, practical and trustworthy. People of this zodiac sign have attachment to material things.',
+  zdone:'Make a hole in copper penny and throw it in flowing water,Donate whole moong,Worship Mother Durga',
+  zplanet:'Mercury',
+  img:'h6.svg'
+},
+{
+  zid:7,
+  zname:'Libra',
+  zelement:'Air',
+  zcolour:'white and light blue',
+  zdigit:6,
+  zdescrption:'People of this zodiac sign are intellectuals, sociable, thinkers, and analysts. People of Air sign enjoy reading books',
+  zdone:'Serve the Cow,Keep home and body clean,Donate Curd,Ghee and Comphore',
+  zplanet:'Venus',
+  img:'h7.svg'
+},
+{
+  zid:8,
+  zname:'Scorpio',
+  zelement:'Water',
+  zcolour:'White, red and all shades of brown',
+  zdigit:9,
+  zdescrption:'People of water zodiac sign are emotional and sensitive and have sharp memory power and are always ready to help their loved ones.',
+  zdone:'Feed roasted garm and jeggery to the monkey on tuesday,Serve elder brother and sister,Donate Whole lantils to the Mars',
+  zplanet:'Mars',
+  img:'h8.svg'
+},
+{
+  zid:9,
+  zname:'Sagittarius',
+  zelement:'Fire',
+  zcolour:'Dark yellow and orange',
+  zdigit:3,
+  zdescrption:'People of this zodiac sign are emotional, dynamic and temperamental and get angry easily. People of the fire zodiac are courageous, energetic and idealistic.',
+  zdone:'Apply Safffron Tilak daily,Donate gram pulses or yellow items',
+  zplanet:'Jupiter',
+  img:'h9.svg'
+},
+{
+  zid:10,
+  zname:'Capricorn',
+  zelement:'Earth',
+  zcolour:'black, purple, dark brown and green',
+  zdigit:1,
+  zdescrption:'People of Earth zodiac sign are down to earth, practical and trustworthy. People of this zodiac sign have attachment to material things.',
+  zdone:'Worship Lord Hanuman and recite Bajarang Ban,Light an lamp on peepal tree on saturn day',
+  zplanet:'Saturn',
+  img:'h10.svg'
+},
+{
+  zid:11,
+  zname:'Aquarius',
+  zelement:'Air',
+  zcolour:'light blue, purple and white',
+  zdigit:4,
+  zdescrption:'People of this zodiac sign are intellectuals, sociable, thinkers, and analysts. People of Air sign enjoy reading books',
+  zdone:'Worship Lord Hanuman and recite Bajarang Ban,Light an lamp on peepal tree on saturn day',
+  zplanet:'Saturn',
+  img:'h11.svg'
+},
+{
+  zid:12,
+  zname:'Pisces',
+  zelement:'Water',
+  zcolour:'yellow and orange',
+  zdigit:3,
+  zdescrption:'People of water zodiac sign are emotional and sensitive and have sharp memory power and are always ready to help their loved ones.',
+  zdone:'Apply Safffron Tilak daily,Donate gram pulses or yellow items',
+  zplanet:'Jupiter',
+  img:'h12.svg'
+}
+]
---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#aj1").click(function(){
+        var newtext="I have been using Astrology Zone for a while now and I must say that I am impressed with the accuracy of their predictions. Their website is user-friendly and easy to navigate. I would highly recommend this website to anyone who is looking for reliable horoscope readings."
+        $("#alltext").html(newtext)
+    });
+    $("#aj2").click(function(){
+        var newtext=" I recently tried Astro Yogendra’s services and I was blown away by the level of detail in their readings. Their astrologers are highly knowledgeable and provide personalized readings that are tailored to your specific needs. I would definitely recommend this website to anyone who is looking for accurate horoscope readings."
+        $("#alltext").html(newtext)
+    }); 
+    $("#aj3").click(function(){
+        var newtext="I have been using Astrology.com for a while now and I must say that I am impressed with the quality of their horoscope readings. Their astrologers are highly skilled and provide accurate predictions that are easy to understand. I would highly recommend this website to anyone who is looking for reliable horoscope readings."
+        $("#alltext").html(newtext)
+    }); 
+    $("#aj4").click(function(){
+        var newtext="I recently tried AstroVed’s services and I was very impressed with the level of detail in their readings. Their astrologers are highly knowledgeable and provide personalized readings that are tailored to your specific needs. I would definitely recommend this website to anyone who is looking for accurate horoscope readings."
+        $("#alltext").html(newtext)
+    });
+     $("#aj5").click(function(){
+        var newtext="“I have been using AstroSage for a few months now and I am very satisfied with their services. Their astrologers are very professional and provide detailed readings that are easy to understand. I would definitely recommend this website to anyone who is looking for reliable horoscope readings."
+        $("#alltext").html(newtext)
+    });
+     $("#aj6").click(function(){
+        var newtext="I have been using AstroSeek for a while now and I must say that I am impressed with the accuracy of their predictions. Their website is user-friendly and easy to navigate. I would highly recommend this website to anyone who is looking for reliable horoscope readings"
+        $("#alltext").html(newtext)
+    }); 
+
+
+    $("#getSign").click(function(){
+        // alert("done")
+    })
+   
+    $("#dely1").click(function(){
+        // alert("done")
+    })
+})
